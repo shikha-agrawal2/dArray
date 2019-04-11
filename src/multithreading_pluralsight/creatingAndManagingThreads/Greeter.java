0479b76aabe2426e74356db1c811fa0c905e1361
Shikha <shikha.agrawal79@gmail.com>
2019-04-12 00:58:38 +0530

    Creating threads and why thread.start() should be called instead of thread.run()

---
+++ src/multithreading_pluralsight/creatingAndManagingThreads/Greeter.java
@@ -0,0 +1,25 @@
+package multithreading_pluralsight.creatingAndManagingThreads;
+/*Creating threads and why thread.start() should be called instead of thread.run(),
+difference between thread.start() where id returned by Thread.currentThread().getId() is always different
+ and thread.run() where id returned by Thread.currentThread().getId() is always same*/
+public class Greeter extends Thread {
+    private String country;
+    public Greeter(String country){
+       // super(country + " thread");
+        this.country = country;
+    }
+    public void run(){
+        //Use Thread.currentThread() to get the Thread instance for the running thread
+        //Why do you think long id = getId() gives a different result
+        long id = Thread.currentThread().getId();
+        System.out.println(id+" : Hello "+ country+ "!");
+    }
+
+    public static void main(String[] args) {
+        String countries[] = {"France", "India", "China", "USA", "Germany"};
+        for(String country : countries){
+            new Greeter(country).start();
+            //new Greeter(country).run();
+        }
+    }
+}
