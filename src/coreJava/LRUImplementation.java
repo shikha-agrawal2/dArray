2fd6b697ee7816778c61e6acc5cb9eb4348d166d
Shikha <shikha.agrawal79@gmail.com>
2019-04-16 19:54:17 +0530

    LRUImplementation

---
+++ src/coreJava/LRUImplementation.java
@@ -0,0 +1,60 @@
+package coreJava;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUImplementation {
+    MyLRU map;
+
+    public LRUImplementation(int capacity) {
+        this.map = new MyLRU(capacity);
+    }
+
+    public int get(int key) {
+        if (map == null || map.get(key) == null) return -1;
+        int value = map.get(key);
+        map.remove(key);
+        map.put(key, value);
+        return value;
+    }
+
+    public void set(int key, int value) {
+        if (map == null) return;
+        get(key);
+        map.put(key, value);
+    }
+
+    public static void main(String[] args) {
+        LRUImplementation lru = new LRUImplementation(4);
+        lru.set(1, 1);
+        lru.set(2, 1);
+        lru.set(3, 1);
+        lru.set(4, 1);
+
+        System.out.println(lru.map);
+
+        lru.set(2, 2);
+
+        System.out.println(lru.map);
+
+        lru.set(5, 1);
+        lru.set(6, 1);
+        lru.set(7, 1);
+
+        System.out.println(lru.map);
+    }
+}
+
+class MyLRU extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+
+    public MyLRU(int capacity) {
+
+        this.capacity = capacity;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry eldest) {
+        return size() > capacity;
+    }
+}
