d35f3b62504c6f537ebef0b297ed8d0118ec6e89
Shikha <shikha.agrawal79@gmail.com>
2019-07-30 01:24:37 +0530

    Count Down Latch Multithreading Example

--- src/multithreading/countDownLatch/ProcessorCDL.java
+++ src/multithreading/countDownLatch/ProcessorCDL.java
@@ -4,11 +4,22 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+//CountDownLatch is used to make sure that a task waits for other threads before it starts.
+//Working of CountDownLatch:
+//When we create an object of CountDownLatch,
+// we specify the number of threads it should wait for, all such thread are required to do count down by calling
+// CountDownLatch.countDown() once they are completed or ready to the job.
+// As soon as count reaches zero, the waiting task starts running.
+
+
+//countDown() method decrements the count and await() method blocks until count == 0
 public class ProcessorCDL implements Runnable {
     private CountDownLatch latch;
-    public ProcessorCDL(CountDownLatch latch){
+
+    public ProcessorCDL(CountDownLatch latch) {
         this.latch = latch;
     }
+
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName());
@@ -23,15 +34,15 @@ public class ProcessorCDL implements Runnable {
 
 }
 
-class App{
+class App {
     public static void main(String[] args) {
         CountDownLatch latch = new CountDownLatch(3);  // CountDownLatch is thread safe
         ExecutorService executor = Executors.newFixedThreadPool(2);
-        for(int i=0; i<4; i++){
+        for (int i = 0; i < 4; i++) {
             executor.submit(new ProcessorCDL(latch));
         }
         try {
-            latch.await();
+            latch.await(); // The main task waits for 3 threads
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
