6e5d35d977c874f8bacf0bb2add25628fbe1c0bc
Shikha <shikha.agrawal79@gmail.com>
2019-08-01 02:35:47 +0530

    TO DO Questions

---
+++ src/linkedlist/AddTwoLists.java
@@ -0,0 +1,10 @@
+package linkedlist;
+
+public class AddTwoLists {
+    static Node head1, head2;
+    public static void main(String[] args) {
+        head1 = new Node(7);
+       // head1.n
+
+    }
+}
---
+++ src/linkedlist/Questions.java
@@ -0,0 +1,6 @@
+package linkedlist;
+
+public class Questions {
+    //https://www.geeksforgeeks.org/write-a-function-to-get-the-intersection-point-of-two-linked-lists/
+    //https://www.geeksforgeeks.org/?p=15194/         - Add two numbers represented by linked lists
+}
