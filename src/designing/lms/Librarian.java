4d94fdb1d320453a1dccf50c64f0eae1ae9d7312
Shikha <shikha.agrawal79@gmail.com>
2019-08-01 02:41:01 +0530

    Library Management System

---
+++ src/designing/lms/Librarian.java
@@ -0,0 +1,27 @@
+package designing.lms;
+
+public class Librarian extends Staff {
+
+    int officeNo;     //Office Number of the Librarian
+    public static int currentOfficeNumber = 0;
+
+    public Librarian(int id,String n, String a, int p, double s,int of) // para cons.
+    {
+        super(id,n,a,p,s);
+
+        if(of == -1)
+            officeNo = currentOfficeNumber;
+        else
+            officeNo = of;
+
+        currentOfficeNumber++;
+    }
+
+    // Printing Librarian's Info
+    @Override
+    public void printInfo()
+    {
+        super.printInfo();
+        System.out.println("Office Number: " + officeNo);
+    }
+}
